18998fa1e0ddf6dedb1d3c2a768b5b3e4d4b27be
Fillippo Nicolini <fn_006@discovery2.hpc.usc.edu>
2023-11-17 13:46:22 -0800

    start map reads

--- pipeline.sh
+++ pipeline.sh
@@ -23,5 +23,8 @@ multiqc -o 01_raw_reads/01_fastqc/ 01_raw_reads/01_fastqc/
 mkdir -p 02_trimmed_reads/01_fastqc
 
 # trim reads
-# REQUIRES trimmomatic and fastqc
+# REQUIRES: conda_envs/readtrimming_env.yml
 for i in 01_raw_reads/SRR*; do python3 scripts/02_trim_reads.py -d $i -adapt 00_input_files/contaminants2trimm.fa; done
+
+# aggregate fastqc report into a single html file
+multiqc -o 02_trimmed_reads/01_fastqc 02_trimmed_reads/01_fastqc
