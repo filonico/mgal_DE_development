f7af50c878cfd1eb5d89cfafcb3cfe07ad96e0f4
Filippo Nicolini <fn76@alice-login01.cm.cluster>
2026-02-03 18:52:28 +0000

    add code to plot genes

--- scripts/07_DE_timeseries.R
+++ scripts/07_DE_timeseries.R
@@ -1,12 +1,16 @@
 #!/usr/bin/env Rscript
 
-if (!requireNamespace("BiocManager", quietly=TRUE))
+if (!requireNamespace("BiocManager", quietly=TRUE)) {
   BiocManager::install("maSigPro")
+}
 
 library(tidyr)
 library(dplyr)
 library(tibble)
 library(ggplot2)
+library(maSigPro)
+
+setwd("/data/evassvis/fn76/mgal_DE_development")
 
 
 #####################
@@ -71,7 +75,7 @@ timeseries_maSigPro <- function(norm_counts, edesign) {
 
 
 # function to plot expression values of selected genes
-plot_expression_values <- function(counts,gene_list,gene_names,edesign) {
+plot_expression_values <- function(counts, gene_list, gene_names, edesign) {
   
   # get gene expression values
   expr <- as_tibble(counts[gene_list,], rownames = NA)
@@ -80,11 +84,12 @@ plot_expression_values <- function(counts,gene_list,gene_names,edesign) {
   expr_to_plot <- rownames_to_column(expr, var = "cds") %>%                     # include rownames in tibble
     pivot_longer(cols = -c(cds), names_to = "run") %>%                          # make pivot longer
     left_join(rownames_to_column(edesign, var = "run")) %>%                     # add information on hours
-    select(-c("Replicate","Group")) %>%                                         # drop unwanted columns
-    left_join(gene_names, keep = FALSE) %>%                                     # add information on gene names
-    drop_na()                                                                   # drop rows with NAs
+    select(-c("Replicate", "Group")) %>%                                        # drop unwanted columns
+    left_join(gene_names, keep = FALSE)                                         # drop rows with NAs
   
-  expr_to_plot$name <- factor(expr_to_plot$name, levels = c("Vasa", "Dmrt1L", "SoxH", "FoxL2", "Wnt8a", "FoxB2"))
+  # expr_to_plot$name <- factor(expr_to_plot$name, levels = c("Vasa", "Piwia", "Piwib", "Nanos", "spPHI",
+  #                                                           "Dmrt1L", "SoxH", "FoxL2",
+  #                                                           "Wnt8a", "FoxB2"))
   
   plot <- ggplot(data = expr_to_plot, aes(x = as.factor(Time), y = value, color = name, linetype = name)) +
     stat_summary(aes(group = gene), fun = mean, geom = "line", linewidth = 0.8) +
@@ -96,7 +101,41 @@ plot_expression_values <- function(counts,gene_list,gene_names,edesign) {
 }
 
 
-#------ACTUAL CODE------------------------------------------------------------
+############################
+#     THEMES FOR PLOTS     #
+############################
+
+theme_for_plots <- theme(
+  # aspect.ratio = 1,
+  plot.background = element_rect(fill = "transparent", colour = NA), 
+  panel.background = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_line(color = "grey90", lineend = "round"),
+  panel.border = element_rect(colour = "black", linewidth = .6),
+  legend.background = element_rect(fill = "transparent", colour = NA),
+  legend.key = element_rect(fill = "transparent", colour = NA),
+  legend.key.width = unit(.4, "cm"),
+  legend.key.height = unit(.4, "cm"),
+  legend.position = "right",
+  legend.title = element_text(face = "bold"),
+  # plot.title = element_text(size = 13, hjust = 0.0, vjust = 1.75, face = "bold"),
+  axis.line = element_blank(),
+  axis.ticks = element_line(colour = "black", linewidth = .4),
+  axis.ticks.length = unit(0.10, "cm"),
+  axis.text.x = element_text(color = "black",
+                             margin = margin(t = 4, r = 0, b = 0, l = 0)),
+  axis.text.y = element_text(color = "black",
+                             margin = margin(t = 0, r = 4, b = 0, l = 0)),
+  axis.title.y = element_text(angle = 90, size = 13,
+                              margin = margin(t = 0, r = 10, b = 0, l = 0)),
+  axis.title.x = element_text(angle = 0, size = 13,
+                              margin = margin(t = 10, r = 0, b = 0, l = 0)),
+  strip.text = element_text(color = "black", face = "bold", hjust = 0),
+  strip.placement = "outside"
+  # strip.background = element_rect(color = "black", linewidth = .6, linetype = "solid"),
+  # strip.text.x = element_text(color = "black")
+)
+  
 
 ########################################
 #     RUN READ COUNT NORMALIZATION     #
@@ -158,6 +197,10 @@ save.image(file = "05_masigpro_analysis/DE_timeseries.Rdata")
 
 # # vst norm, all timepoints
 # vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B017427"] # vasa:    no
+# vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B086491"] # nanos:    no
+# vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B073040"] # piwia:    no
+# vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B064020"] # piwib:    no
+# vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B067558"] # spPHI:    no
 # vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B093608"] # dmrt1L:  no
 # vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B014180"] # soxH:    no
 # vstnorm_alltimepoints$visualization_genes$cut["gene-MGAL_10B094018"] # foxL2:   YES, cluster 3
@@ -165,6 +208,10 @@ save.image(file = "05_masigpro_analysis/DE_timeseries.Rdata")
 
 # # vst norm, all timepoints
 # vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI03912.1"] # vasa:    YES, cluster 3
+# vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI80212.1"] # nanos:    YES, cluster 3
+# vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI60324.1"] # piwia:    no
+# vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI83778.1"] # piwib:    YES, cluster 2
+# vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI56617.1"] # spPHI:    YES, cluster 2
 # vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI03798.1"] # dmrt1L:  no
 # vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI30824.1"] # soxH:    no
 # vstnorm_alltimepoints$visualization_genes$cut["Mgal_VDI49864.1"] # foxL2:   YES, cluster 4
@@ -197,33 +244,28 @@ save.image(file = "05_masigpro_analysis/DE_timeseries.Rdata")
 #     PLOT GENE EXPRESSION     #
 ################################
 
+load(file = "05_masigpro_analysis/DE_timeseries.Rdata")
+
 # generate gene conversion dataframe
-gene_names <- data.frame(gene = c("gene-MGAL_10B017427", "gene-MGAL_10B086491", "gene-MGAL_10B073040", "gene-MGAL_10B064020", "gene-MGAL_10B093608","gene-MGAL_10B014180","gene-MGAL_10B094018","gene-MGAL_10B085403","gene-MGAL_10B093191"),
-                         stringtie = c("MSTRG.9718", NA, NA, NA, "MSTRG.9631", "MSTRG.26773", "MSTRG.38958", "MSTRG.41862", "MSTRG.30071"),
-                         cds = c("Mgal_VDI03912.1", "Mgal_VDI80212.1", "Mgal_VDI60324.1", "Mgal_VDI83778.1", "Mgal_VDI03798.1", "Mgal_VDI30824.1", "Mgal_VDI49864.1", "Mgal_VDI54402.1", "Mgal_VDI35942.1"),
-                         name = c("Vasa", "Nanos", "Piwia", "Piwib", "Dmrt1L", "SoxH", "FoxL2", "Wnt8a", "FoxB2"))
+gene_names <- data.frame(gene = c("gene-MGAL_10B017427", "gene-MGAL_10B086491", "gene-MGAL_10B073040", "gene-MGAL_10B064020", "gene-MGAL_10B067558", "gene-MGAL_10B093608","gene-MGAL_10B014180","gene-MGAL_10B094018","gene-MGAL_10B085403","gene-MGAL_10B093191"),
+                         stringtie = c("MSTRG.9718", NA, NA, NA, NA, "MSTRG.9631", "MSTRG.26773", "MSTRG.38958", "MSTRG.41862", "MSTRG.30071"),
+                         cds = c("Mgal_VDI03912.1", "Mgal_VDI80212.1", "Mgal_VDI60324.1", "Mgal_VDI83778.1", "Mgal_VDI56617.1", "Mgal_VDI03798.1", "Mgal_VDI30824.1", "Mgal_VDI49864.1", "Mgal_VDI54402.1", "Mgal_VDI35942.1"),
+                         name = c("Vasa", "Nanos", "Piwia", "Piwib", "spPHI", "Dmrt1L", "SoxH", "FoxL2", "Wnt8a", "FoxB2"))
 
 
 vst.expr.plot <- plot_expression_values(vstnorm_alltimepoints$counts,
-                                        gene_names$cds,
+                                        c("Mgal_VDI03912.1", "Mgal_VDI80212.1", "Mgal_VDI56617.1"),
+                                        # gene_names$cds,
                                         gene_names,
                                         vstnorm_alltimepoints$edesign) +
-  scale_colour_manual(values = c("Red", "Cyan", "Magenta", "Yellow", "Grey", "Grey")) +
-  scale_linetype_manual(values = c(rep("solid", 4), rep("dashed", 2))) +
+  # scale_colour_manual(values = c("Red", "Cyan", "Magenta", "Yellow", "Grey", "Grey")) +
+  # scale_linetype_manual(values = c(rep("solid", 4), rep("dashed", 2))) +
   ggtitle("Vst transformed gene expression") +
   labs(color = "Gene", linetype = "Gene") +
-  theme_bw() +
-  theme(panel.grid = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_rect(fill = 'transparent'),
-        plot.background = element_rect(fill = 'transparent', color = NA),
-        axis.line = element_line(linewidth = 0.8),
-        axis.ticks = element_line(color = "black", size = 0.8),
-        axis.text = element_text(color = "black"),
-        legend.background = element_rect(fill = 'transparent', color = NA),
-        legend.text = element_text(face = "bold"))
+  theme_bw(base_size = 12) +
+  theme_for_plots
 
-# vst.expr.plot
+vst.expr.plot
 
 # ggsave("03b_mapped_reads_BOWTIE/vstgene_expression.pdf", plot = vst.expr.plot, device = "pdf",
        # dpi = 300, height = 4.7, width = 6, units = ("in"), bg = 'transparent')
@@ -244,3 +286,125 @@ vst.expr.plot <- plot_expression_values(vstnorm_alltimepoints$counts,
                            # ncol = 2, nrow = 1,
                            # common.legend = TRUE, legend = "right")
 # panel
+
+
+##################################
+#     PLOT MASIGPRO CLUSTERS     #
+##################################
+
+labeller_cluster <- vstnorm_alltimepoints$visualization_genes$cut %>%
+  tibble(group = .) %>%
+  group_by(group) %>%
+  summarise(n_genes = n()) %>%
+  ungroup() %>%
+  mutate(label = paste0("Cluster ", group,
+                        " (", format(n_genes, big.mark = ","), " genes)")) %>%
+  select(-n_genes) %>%
+  deframe() %>%
+  as_labeller()
+
+data_to_plot <- vstnorm_alltimepoints$visualization_genes$cut %>%
+  tibble(gene = names(.),
+         group = .) %>%
+  left_join(vstnorm_alltimepoints$counts %>%
+              rownames_to_column(var = "gene")) %>%
+  pivot_longer(-c(gene, group), names_to = "sample", values_to = "profile") %>%
+  left_join(vstnorm_alltimepoints$sig_genes_list$sig.genes$edesign %>%
+              rownames_to_column(var = "sample") %>%
+              select(sample, Time))
+
+# gene-level mean per timepoint (grey lines)
+gene_mean <- data_to_plot %>%
+  group_by(gene, group, Time) %>%
+  summarise(profile_mean = mean(profile), .groups = "drop") %>%
+  mutate(line_type = case_when(gene == "Mgal_VDI03912.1" ~ "vasa",
+                               gene == "Mgal_VDI80212.1" ~ "nanos",
+                               gene == "Mgal_VDI83778.1" ~ "piwi-b",
+                               gene == "Mgal_VDI56617.1" ~ "spPHI",
+                               TRUE ~ "Other genes")) %>%
+  mutate(line_type = factor(line_type,
+                            levels = c("vasa", "nanos", "piwi-b", "spPHI", "Other genes", "Mean expression")))
+
+# group-level mean per timepoint (red line)
+group_mean <- data_to_plot %>%
+  group_by(group, Time) %>%
+  summarise(mean_profile = mean(profile),
+            sd_profile   = sd(profile),
+            .groups = "drop") %>%
+  mutate(ymin = mean_profile - sd_profile,
+         ymax = mean_profile + sd_profile,
+         line_type = "Mean expression")
+
+masigpro_groups_panel <- ggplot() +
+  
+  # grey gene lines
+  geom_line(data = gene_mean,
+            aes(x = Time, y = profile_mean, group = gene, color = line_type),
+            alpha = 0.3, linewidth = 0.3, lineend = "round") +
+  
+  # # ribbon around group mean
+  # geom_ribbon(data = group_mean,
+  #             aes(x = Time, ymin = ymin, ymax = ymax, group = group),
+  #             fill = "azure4", alpha = 0.2) +
+  
+  # mean line
+  geom_line(data = group_mean,
+            aes(x = Time, y = mean_profile, group = group, color = line_type),
+            linewidth = 0.3, linetype = "dashed", lineend = "round") +
+  
+  # vasa line
+  geom_line(data = gene_mean %>%
+              filter(gene == "Mgal_VDI03912.1"),
+            aes(x = Time, y = profile_mean, group = gene, color = line_type),
+            linewidth = 0.5, lineend = "round") +
+  
+  # nanos line
+  geom_line(data = gene_mean %>%
+              filter(gene == "Mgal_VDI80212.1"),
+            aes(x = Time, y = profile_mean, group = gene, color = line_type),
+            linewidth = 0.5, lineend = "round") +
+  
+  # piwib line
+  geom_line(data = gene_mean %>%
+              filter(gene == "Mgal_VDI83778.1"),
+            aes(x = Time, y = profile_mean, group = gene, color = line_type),
+            linewidth = 0.5, lineend = "round") +
+  
+  # spPHI line
+  geom_line(data = gene_mean %>%
+              filter(gene == "Mgal_VDI56617.1"),
+            aes(x = Time, y = profile_mean, group = gene, color = line_type),
+            linewidth = 0.5, lineend = "round") +
+  
+  scale_color_manual(name = "Line type",
+                     values = c("Other genes" = "azure3",
+                                "vasa" = "firebrick1",
+                                "nanos" = "chocolate1",
+                                "piwi-b" = "darkorchid1",
+                                "spPHI" = "deeppink1",
+                                "Mean expression" = "azure4")) +
+  
+  scale_x_continuous(breaks = unique(gene_mean$Time), expand = c(0, 0)) +
+  
+  facet_wrap(~group, labeller = labeller_cluster) +
+  
+  xlab("Hours post fertilization (hpf)") +
+  ylab("Normalized expression value") +
+  
+  theme_minimal(base_size = 8) +
+  theme_for_plots +
+  theme(panel.spacing.x = unit(1, "lines"),
+        panel.spacing.y = unit(0.4, "lines"))
+
+masigpro_groups_panel
+
+
+ggsave("05_masigpro_analysis/vstnorm_alltimepoints_maSigPro_clusters.pdf",
+       plot = masigpro_groups_panel, device = "pdf",
+       dpi = 300, height = 6, width = 9, units = ("in"), bg = "white")
+
+ggsave("05_masigpro_analysis/vstnorm_alltimepoints_maSigPro_clusters.png",
+       plot = masigpro_groups_panel, device = "png",
+       dpi = 300, height = 6, width = 10, units = ("in"), bg = "white")
+  
+save.image(file = "05_masigpro_analysis/DE_timeseries.Rdata")
